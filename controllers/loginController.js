6aba8e5328c36e3d4c075db27f05b4d98f7a993b
juanabernstein <jbernstein@udesa.edu.ar>
2025-04-04 19:51:51 -0300

    cree el formulario de login

---
+++ controllers/loginController.js
@@ -0,0 +1,8 @@
+
+// let info = require(..////)
+
+const productos = {
+    login : function(req, res, next) {
+        res.render ('login') // completar segunda parte de esta ruta cuando hagamos el db importando la info!!
+       
+    } , }
