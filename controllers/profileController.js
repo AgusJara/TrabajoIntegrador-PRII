c23f8a3c0a9653acd7f6485c5e605f4252284ace
AgusJara <ajaralambides@udesa.edu.ar>
2025-04-04 22:33:02 -0300

    Hice usuarios.js con los datos del usuario y los agregue a profile.ejs al igual que a headerLogueado. No se si falta alguna info en alguna otra vista, capaz falta cambios

---
+++ controllers/profileController.js
@@ -0,0 +1,7 @@
+const profile = require('../db/usuario');
+const profileController = {
+    info: function (req,res) {
+        res.render('profile',{profile:profile.lista})
+    }
+};
+    
--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const profileController = require ('../controllers/profileController')
 
-router.get('/', profileController)
+router.get('/', profileController.info)
 
 module.exports = router;
 
