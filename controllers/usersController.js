041edfd23266e62cef2e9e2f2b3d37c1ac854b42
Delfi Camuglia <delficamuglia@Mac.fibertel.com.ar>
2025-05-25 17:13:30 -0300

    Haciendo el modelo de users y haciendo el register: el email no puede estar vacio ni repetido, la contrasenia debe tener al menos 3 caracteres, y debe estar encriptada, etc

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,18 @@
-let datos = require('../db/datos');
+let datos= require('../db/datos')
+let bcrypt= require('bcryptjs')
+
+const db= require('../database/models')
+let users= db.User
+
 let usersController = {
     index: function(req, res) {
-        res.render('profile',{profile: datos.usuario[0],
-                              producto: datos.productos});
+        users.findAll()
+        .then(function(data){
+            //return res.send(data)
+            res.render('profile',{profile: data[0],
+                producto: datos.productos});
+        })
+        
     },
    login: function (req,res,) {
     res.render ('login')
@@ -10,8 +20,41 @@ let usersController = {
    },
     register: function (req,res) {
         res.render('register',{profile: datos.usuario[0], productos: datos.usuario[0]})
-    }
+    },
+    show: function (req, res){
+       if (req.body.email == ''){
+        return res.send('El email no puede estar vacio.')
+       }
+
+       if (req.body.constrasenia.length < 3) {
+         return res.send('La contraseña debe tener al menos 3 caracteres.');
+        }
+
+       db.User.findOne ({ where: [{email: req.body.email}] })
+            .then (function(userExistente){
+                if (userExistente){
+                    return res.send('El email ya esta registrado.')
+                }
+                
+                return db.User.create({
+                    nombre: req.body.usuario,
+                    email: req.body.email,
+                    constrasenia: bcrypt.hashSync(req.body.constrasenia, 10),
+                    fechaDeNacimiento: req.body.fechaNacimiento, 
+                    nroDeDocumento: req.body.documento,
+                });
+            })
+        .then(function(nuevoUsuario){
+            if (nuevoUsuario){
+                res.redirect('/users')
+            }
+        })
+        .catch(function(error){
+            return res.send(error)
+        })
+    },
 }
 
 module.exports = usersController;
+
 
---
+++ database/models/User.js
@@ -0,0 +1,47 @@
+module.exports= function(Sequelize, DataTypes) {
+    let alias= 'User'
+    let cols= {
+        id: {
+            primaryKey: true, 
+            autoIncrement: true,
+            type: DataTypes.INTEGER.UNSIGNED,
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            notNull: true,
+            unique: true,
+        },
+        constrasenia: {
+            type: DataTypes.STRING,
+            notNull: true,
+        },
+        fechaDeNacimiento: {
+            notNull: true,
+            type: DataTypes.DATE,
+        },
+        nroDeDocumento: {
+            type: DataTypes.INTEGER,
+            unique: true,
+        },
+        fotoDePerfil: {
+            type: DataTypes.STRING,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+        },
+        deletedAt: {
+            type: DataTypes.DATE,
+        },
+    }
+
+    let config= {
+        tableName: 'users',
+        timestamps: true,
+        underscored: false,
+    }
+    const User= Sequelize.define(alias, cols, config)
+    return User;
+}
